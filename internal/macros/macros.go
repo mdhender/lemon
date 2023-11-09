b4947dc7f493428030eebc014af0afce106b4188
Michael Henderson <mdhender@mdhender.com>
2023-11-09 16:15:51 -0700

    fixed test cases for macros package

--- internal/macros/macros.go
+++ internal/macros/macros.go
@@ -9,6 +9,156 @@ import (
 	"unicode"
 )
 
+// EvalExpression evaluates a macro expression. A macro is considered
+// to be true if it is defined with -D on the command line; otherwise
+// it is considered to be false. `symtab` is the table of macros that
+// were defined on the command line.
+func EvalExpression(input []byte, symtab map[string]string) (bool, error) {
+	expression, err := parseExpression(input)
+	if err != nil {
+		return false, err
+	}
+	result, err := evalExpression(expression, symtab)
+	if err != nil {
+		return false, err
+	}
+	return result, nil
+}
+
+func evalExpression(expression []token, symtab map[string]string) (bool, error) {
+	if len(expression) == 0 {
+		return false, nil
+	}
+	var stack []bool
+	for _, tok := range expression {
+		switch tok.kind {
+		case cTrue:
+			stack = append(stack, true)
+		case cFalse:
+			stack = append(stack, false)
+		case cVariable:
+			if _, ok := symtab[string(tok.value)]; ok {
+				stack = append(stack, true)
+			} else {
+				stack = append(stack, false)
+			}
+		case cAnd:
+			if len(stack) < 2 {
+				return false, fmt.Errorf("%d: unexpected '&&'", tok.start)
+			}
+			rh := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+			lh := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+			stack = append(stack, lh && rh)
+		case cOr:
+			if len(stack) < 2 {
+				return false, fmt.Errorf("%d: unexpected '||'", tok.start)
+			}
+			rh := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+			lh := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+			stack = append(stack, lh || rh)
+		case cNot:
+			if len(stack) == 0 {
+				return false, fmt.Errorf("%d: unexpected '!'", tok.start)
+			}
+			lh := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+			stack = append(stack, !lh)
+		default:
+			return false, fmt.Errorf("%d: unexpected %q", tok.start, tok)
+		}
+	}
+	if len(stack) == 0 {
+		// should never happen
+		return false, fmt.Errorf("0: empty stack")
+	} else if len(stack) != 1 {
+		return false, fmt.Errorf("0: error evaluating")
+	}
+	return stack[0], nil
+}
+
+func parseExpression(input []byte) ([]token, error) {
+	// parse the input (which is an infix boolean expression) into tokens
+	var infix []token
+	for pos := 0; pos < len(input); {
+		if unicode.IsSpace(rune(input[pos])) {
+			pos++
+		} else if isalpha(input[pos]) {
+			start := pos
+			for pos < len(input) && (isident(input[pos])) {
+				pos++
+			}
+			infix = append(infix, token{start: start + 1, kind: cVariable, value: input[start:pos]})
+		} else if input[pos] == '(' {
+			infix = append(infix, token{start: pos + 1, kind: cGroupStart})
+			pos++
+		} else if input[pos] == ')' {
+			infix = append(infix, token{start: pos + 1, kind: cGroupEnd})
+			pos++
+		} else if input[pos] == '!' {
+			infix = append(infix, token{start: pos + 1, kind: cNot})
+			pos++
+		} else if bytes.HasPrefix(input[pos:], []byte{'&', '&'}) {
+			infix = append(infix, token{start: pos + 1, kind: cAnd})
+			pos += 2
+		} else if bytes.HasPrefix(input[pos:], []byte{'|', '|'}) {
+			infix = append(infix, token{start: pos + 1, kind: cOr})
+			pos += 2
+		} else {
+			text := input[pos:]
+			if len(text) > 8 {
+				text = text[:8]
+			}
+			return nil, fmt.Errorf("%d: unexpected input %q", pos+1, string(text))
+		}
+	}
+
+	// convert infix to postfix
+	var postfix, operators []token
+	for _, tok := range infix {
+		switch tok.kind {
+		case cVariable:
+			postfix = append(postfix, tok)
+			if len(operators) != 0 {
+				op := operators[len(operators)-1]
+				if op.kind != cGroupStart {
+					// pop the operator and push it onto the postfix stack
+					postfix, operators = append(postfix, op), operators[:len(operators)-1]
+				}
+			}
+		case cAnd, cOr, cNot:
+			operators = append(operators, tok)
+		case cGroupStart:
+			operators = append(operators, tok)
+		case cGroupEnd:
+			var op token
+			for len(operators) != 0 {
+				op, operators = operators[len(operators)-1], operators[:len(operators)-1]
+				if op.kind == cGroupStart {
+					break
+				}
+				postfix = append(postfix, op)
+			}
+			if op.kind != cGroupStart {
+				return nil, fmt.Errorf("%d: unbalanced ')'", tok.start)
+			}
+		}
+	}
+	for len(operators) != 0 {
+		op := operators[len(operators)-1]
+		if op.kind == cGroupStart {
+			return nil, fmt.Errorf("%d: unbalanced '('", op.start)
+		}
+		postfix = append(postfix, op)
+		operators = operators[:len(operators)-1]
+	}
+
+	return postfix, nil
+}
+
 type cell int
 
 const (
@@ -57,29 +207,6 @@ func (t token) String() string {
 	return fmt.Sprintf("{%d %s %q}", t.start, t.kind, string(t.value))
 }
 
-type expr struct {
-	kind     string
-	value    []byte
-	variable []byte
-	and, or  struct {
-		lh, rh *expr
-	}
-	not   *expr
-	group *expr
-}
-
-func (e *expr) String() string {
-	if e == nil {
-		return "nil"
-	} else if e.value != nil {
-		return fmt.Sprintf("value(%q)", string(e.value))
-	} else if e.variable != nil {
-		return fmt.Sprintf("variable(%q)", string(e.variable))
-	} else {
-		return "expr{...}"
-	}
-}
-
 func isalpha(ch byte) bool {
 	return ('a' <= ch && ch <= 'z') || ('A' <= ch && ch <= 'Z')
 }
@@ -87,207 +214,3 @@ func isalpha(ch byte) bool {
 func isident(ch byte) bool {
 	return ('a' <= ch && ch <= 'z') || ('A' <= ch && ch <= 'Z') || ('0' <= ch && ch <= '9') || ch == '_'
 }
-
-func evalMacro(input []byte, symtab map[string]string) (bool, error) {
-
-	return false, fmt.Errorf("not implemented")
-}
-
-func parseExpression(input []byte) ([]token, error) {
-	var postfix, infix, operators []token
-
-	// parse the input (which is a postfix boolean expression) into tokens
-	for pos := 0; pos < len(input); {
-		if unicode.IsSpace(rune(input[pos])) {
-			pos++
-		} else if isalpha(input[pos]) {
-			start := pos
-			for pos < len(input) && (isident(input[pos])) {
-				pos++
-			}
-			postfix = append(postfix, token{start: start + 1, kind: cVariable, value: input[start:pos]})
-		} else if input[pos] == '(' {
-			postfix = append(postfix, token{start: pos + 1, kind: cGroupStart})
-			pos++
-		} else if input[pos] == ')' {
-			postfix = append(postfix, token{start: pos + 1, kind: cGroupEnd})
-			pos++
-		} else if input[pos] == '!' {
-			postfix = append(postfix, token{start: pos + 1, kind: cNot})
-			pos++
-		} else if bytes.HasPrefix(input[pos:], []byte{'&', '&'}) {
-			postfix = append(postfix, token{start: pos + 1, kind: cAnd})
-			pos += 2
-		} else if bytes.HasPrefix(input[pos:], []byte{'|', '|'}) {
-			postfix = append(postfix, token{start: pos + 1, kind: cOr})
-			pos += 2
-		} else {
-			text := input[pos:]
-			if len(text) > 8 {
-				text = text[:8]
-			}
-			return nil, fmt.Errorf("%d: unexpected input %q", pos+1, string(text))
-		}
-	}
-
-	// convert the postfix expression into infix expression
-	for _, tok := range postfix {
-		switch tok.kind {
-		case cAnd:
-			operators = append(operators, tok)
-		case cGroupEnd:
-			if len(operators) == 0 {
-				return nil, fmt.Errorf("%d: unexpected ')'", tok.start)
-			}
-			op := operators[len(operators)-1]
-			if op.kind == cGroupStart {
-				operators = operators[:len(operators)-1]
-			} else {
-				fmt.Printf("operators")
-				for _, op := range operators {
-					switch op.kind {
-					case cAnd:
-						fmt.Print(" &&")
-					case cGroupEnd:
-						fmt.Print(" ) ")
-					case cGroupStart:
-						fmt.Print(" ( ")
-					case cNot:
-						fmt.Print(" ! ")
-					case cOr:
-						fmt.Print(" ||")
-					default:
-						fmt.Print(op)
-					}
-				}
-				fmt.Println("")
-				return nil, fmt.Errorf("%d: unbalanced ')'", tok.start)
-			}
-		case cGroupStart:
-			operators = append(operators, tok)
-		case cNot:
-			operators = append(operators, tok)
-		case cOr:
-			operators = append(operators, tok)
-		case cVariable:
-			infix = append(infix, tok)
-			if len(operators) != 0 {
-				op := operators[len(operators)-1]
-				if op.kind != cGroupStart {
-					infix, operators = append(infix, op), operators[:len(operators)-1]
-				}
-			}
-		}
-	}
-	for len(operators) != 0 {
-		op := operators[len(operators)-1]
-		if op.kind == cGroupStart {
-			return nil, fmt.Errorf("%d: unbalanced '('", op.start)
-		}
-		infix, operators = append(infix, op), operators[:len(operators)-1]
-	}
-
-	return infix, nil
-}
-
-// variables is the symbol table for macros.
-func parseMacroExpression(input []byte, variables map[string]string) ([]byte, error) {
-	// Create a stack to store the operands and operators
-	var stack []byte
-	var operators []byte
-
-	var priorCell cell
-
-	// Iterate over the postfix expression from left to right
-	for pos, token := range input {
-		// If the token is an operand, push it onto the stack
-		if token == '(' {
-			operators = append(operators, token)
-		} else if token == ')' {
-			if len(operators) == 0 {
-				return nil, fmt.Errorf("%d: unexpected ')'", pos+1)
-			} else if operators[len(operators)-1] != '(' {
-				fmt.Printf("error: unbalanced ')': operators are %q\n", string(operators))
-				return nil, fmt.Errorf("%d: unbalanced ')'", pos+1)
-			}
-			operators = operators[:len(operators)-1]
-		} else if token == '&' {
-			switch priorCell {
-			case cTrue, cFalse, cVariable:
-				// okay
-			case cNone, cAnd, cOr, cNot:
-				return nil, fmt.Errorf("%d: unexpected operator %q", pos+1, string(token))
-			}
-			operators = append(operators, token)
-			priorCell = cAnd
-		} else if token == '|' {
-			switch priorCell {
-			case cTrue, cFalse, cVariable:
-				// okay
-			case cNone, cAnd, cOr, cNot:
-				return nil, fmt.Errorf("%d: unexpected operator %q", pos+1, string(token))
-			}
-			operators = append(operators, token)
-			priorCell = cOr
-		} else if token == '!' {
-			switch priorCell {
-			case cNone, cAnd, cOr, cNot:
-				// okay
-			case cTrue, cFalse, cVariable:
-				return nil, fmt.Errorf("%d: unexpected operator %q", pos+1, string(token))
-			}
-			operators = append(operators, token)
-			priorCell = cNot
-		} else if 'a' <= token && token <= 'z' {
-			switch priorCell {
-			case cTrue, cFalse, cVariable:
-				return nil, fmt.Errorf("%d: unexpected variable %q", pos+1, string(token))
-			case cNone, cAnd, cOr, cNot:
-				// okay
-			}
-			var variable byte
-			if _, ok := variables[string(token)]; ok {
-				//variable = 'T'
-				variable = token
-			} else {
-				//variable = 'F'
-				variable = token
-			}
-			stack = append(stack, variable)
-			if len(operators) != 0 && operators[len(operators)-1] != '(' {
-				stack, operators = append(stack, operators[len(operators)-1]), operators[:len(operators)-1]
-			}
-			priorCell = cVariable
-		} else {
-			return nil, fmt.Errorf("%d: unknown token %q", pos+1, string(token))
-		}
-	}
-
-	for len(operators) != 0 {
-		if operators[len(operators)-1] == '(' {
-			return nil, fmt.Errorf("%d: unbalanced '('", len(input))
-		}
-		stack, operators = append(stack, operators[len(operators)-1]), operators[:len(operators)-1]
-	}
-
-	return stack, nil
-}
-
-//func isOperand(token byte) bool {
-//	return token == 't' || token == 'f'
-//}
-
-//func calculate(operand1, operand2, operator string) string {
-//	var result string
-//	switch operator {
-//	case "+":
-//		result = operand1 + operand2
-//	case "-":
-//		result = operand1 - operand2
-//	case "*":
-//		result = operand1 * operand2
-//	case "/":
-//		result = operand1 / operand2
-//	}
-//	return result
-//}
--- internal/macros/macros_test.go
+++ internal/macros/macros_test.go
@@ -1,10 +1,11 @@
 // lemon - a parser generator
 // Copyright (c) 2023 Michael D Henderson. All rights reserved.
 
-package macros
+package macros_test
 
 import (
 	"fmt"
+	"github.com/mdhender/lemon/internal/macros"
 	"testing"
 )
 
@@ -12,27 +13,43 @@ func TestParseMacroExpression(t *testing.T) {
 	variables := make(map[string]string)
 	variables["a"] = "true"
 	variables["b"] = "true"
-	for i, expr := range []string{
-		"a",
-		"a && b",
-		"a && (b || c)",
-		"!a",
-		"!a && !b",
-		"( a ) && ! ( b )",
-		"a || b || (((c)))",
-		"!(c)",
-		"!(!c)",
-		"!(!(c))",
+
+	type test_case struct {
+		id     int
+		input  string
+		expect bool
+		err    error
+	}
+	for _, tc := range []test_case{
+		{id: 1, input: "a", expect: true},
+		{id: 2, input: "b", expect: true},
+		{id: 3, input: "c", expect: false},
+		{id: 4, input: "a && b", expect: true},
+		{id: 5, input: "a && c", expect: false},
+		{id: 6, input: "!a", expect: false},
+		{id: 7, input: "!a || !c", expect: true},
+		{id: 8, input: "( a ) && ! ( b )", expect: false},
+		{id: 9, input: "a || b || (((c)))", expect: true},
+		{id: 10, input: "!(c)", expect: true},
+		{id: 11, input: "!(!c)", expect: false},
+		{id: 12, input: "!(!(c))", expect: false},
+		{id: 13, input: ")", err: fmt.Errorf("1: unbalanced ')'")},
 	} {
-		fmt.Printf("%2d: %q\n", i+1, expr)
-		result, err := parseExpression([]byte(expr))
-		if err != nil {
-			fmt.Printf("%2d: --- error: %+v\n", i+1, err)
-		} else {
-			// print the result
-			for _, tok := range result {
-				fmt.Printf("%2d: ... token %s\n", i+1, tok)
+		//fmt.Printf("%2d: %q\n", tc.id, tc.input)
+		got, err := macros.EvalExpression([]byte(tc.input), variables)
+		if tc.err != nil {
+			if err != nil {
+				want, got := tc.err.Error(), err.Error()
+				if want != got {
+					t.Errorf("%2d: want %v: got %+v\n", tc.id, tc.err, err)
+				}
+			} else {
+				t.Errorf("%2d: want %v: got success\n", tc.id, tc.err)
 			}
+		} else if err != nil {
+			t.Errorf("%2d: want success: got %+v\n", tc.id, err)
+		} else if tc.expect != got {
+			t.Errorf("%2d: want %v: got %v\n", tc.id, tc.expect, got)
 		}
 	}
 }
